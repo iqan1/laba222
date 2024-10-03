a147e1adca59150669439268dc80e4d611e86848
iqan1 <ivan777kolt@gmail.com>
2024-10-03 14:47:23 +0300

    Update sum_before_and_after.c

--- sum_before_and_after.c
+++ sum_before_and_after.c
@@ -1,5 +1,6 @@
 #include "sum_before_and_after.h"
-#include <stdlib.h>
+#include "index_first_zero.h"
+#include "index_last_zero.h"
 
 int sum_before_and_after(int arr[], int len, int first_zero, int last_zero) {
     int sum = 0;
