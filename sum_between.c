4e6e7b2f8771d78a72d39c019adbca5ceb0b63a5
iqan1 <ivan777kolt@gmail.com>
2024-10-03 14:46:43 +0300

    Update sum_between.c

--- sum_between.c
+++ sum_between.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "sum_between.h"
+#include "index_first_zero.h"
+#include "index_last_zero.h"
 int sum_between(int arr[], int len, int first_zero, int last_zero) {
     int sum = 0;
     for (int i = first_zero + 1; i < last_zero; i++) {
